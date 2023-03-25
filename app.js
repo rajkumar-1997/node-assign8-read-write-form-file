9fad60cccda0cc8f77d7665629a736adfe09d2a3
rajkumar-1997 <124024525+rajkumar-1997@users.noreply.github.com>
2023-03-25 17:58:32 +0530

    readwrite form file

---
+++ app.js
@@ -0,0 +1,66 @@
+const  http=require('http');
+const fs=require('fs');
+const { buffer } = require('stream/consumers');
+
+
+let server=http.createServer((req,res)=>{
+    
+    const url=req.url;
+    method=req.method;
+   
+   
+    if(url==='/'){
+        fs.readFile('message.txt',{encoding:'utf-8'},(err,data)=>{
+            if(err) {
+                console.log(err);
+            }
+            console.log('data from file '+ data);
+            res.write('<html>');
+            res.write('<head><title>Message</title></head>');
+            res.write(`<body>${data}<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></from></body>`);
+            res.write('</html>');
+           
+           return res.end();
+        });
+      
+   
+    }
+    else if (url==='/message' && method==="POST" ){
+       const body=[]
+       req.on('data',(chunk)=>{
+        console.log(chunk);
+          body.push(chunk);
+       });
+       return  req.on('end',()=>{
+        const parsebody=Buffer.concat(body).toString();
+        const message=parsebody.split('=')[1];
+        console.log(parsebody);
+        fs.writeFile('message.txt',message,(err)=>{
+            if(err){
+                console.log(err)
+            }
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+       });
+
+    }
+    
+    else {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>my first page</title></head>');
+        res.write('<body><h1> hello from node.js server</h1></body>');
+        res.write('</html>');
+       
+        res.end();
+    }
+   
+   
+
+});
+
+server.listen(3000,()=>{
+    console.log("server has satrted at port 3000")
+})
